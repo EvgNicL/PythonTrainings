370473b5e0c00458e8d20f455fa4c52ef10722e1
ENL <evgenialisunova520@gmail.com>
2024-12-24 23:18:55 +0400

    changed module_1_5.py

--- 1.5_StringAndStringIndexing/module_1_5.py
+++ 1.5_StringAndStringIndexing/module_1_5.py
@@ -1,6 +1,6 @@
-example = 'Строки и индексация строк'
+example = 'Топинамбур'
 print(example[0])
 print(example[-1])
-print(example[12:])
+print(example[5:])
 print(example[::-1])
-print(example[::2])
+print(example[1::2])
