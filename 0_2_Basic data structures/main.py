60ba9bc7048c3a27127bd4653395b3c67788a09e
ENL <evgenialisunova520@gmail.com>
2024-12-24 21:15:00 +0400

    module_1_3.py

---
+++ 0_2_Basic data structures/main.py
@@ -0,0 +1,22 @@
+print("1st program")
+print(9 ** 0.5 * 5)
+
+print()
+print("2nd program")
+print(9.99 > 9.98 and 1000 != 1001)
+
+print()
+print("3rd program")
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+print()
+print("4th program")
+s = '123. 456'
+s = s[0: 4] + s[5:]
+f = float(s) * 10
+f = int(f)
+f = f % 10
+print(f)
+
